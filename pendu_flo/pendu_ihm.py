3d47f6d0c9152cf452ea6b8268abe60dd985c725
AeRiaSaito <florence.navet@laplateforme.io>
2025-01-22 15:18:39 +0100

    ajout fichier ihm

---
+++ pendu_flo/pendu_ihm.py
@@ -0,0 +1,41 @@
+
+import pygame
+import random
+
+
+# def charger_mots(fichier):
+#     with open(fichier, "r") as file:
+#         mots = file.read().splitlines
+#     return mots
+
+# def ajouter_mots(fichier):
+#     nouveau_mot = input("Entrez un nouveau mot").lower()
+#     if nouveau_mot.isalpha():
+#         with open(fichier, "a") as file:
+#             file.write(nouveau_mot + "\n")
+#             print(f"Le mot {nouveau_mot} a été ajouté avec succès")
+#     else:
+#         print("Veuillez entrez un mot valide.")
+
+
+# def menu_principal(fichier):
+#     while True:
+#         print("\n=== Menu Principal ===")
+#         print("1. Jouer")
+#         print("2.Ajouter un mot")
+#         print("3.Quitter")
+        
+#         choix = input("Choisissez une option (1-3) :")
+
+#         if choix == "1":
+#             jouer(fichier)
+#         elif choix == "2":
+#             ajouter_mots(fichier):
+#         elif choix == "3":
+#             print("Au revoir !")
+#             break
+#         else:
+#             print("Choix invalide. Veuillez réessayez.")
+
+
+def jouer
